97ca03f5f132ff63d5239343d8727de1682e4d7d
Peter <dyvor@ovi.com>
2011-03-02 10:37:38 +0100

    * Port length extended to 5 digits. * Parsing the url path.
    Signed-off-by: Peter <dyvor@ovi.com>

--- plugins/urlParser/url-parser.js
+++ plugins/urlParser/url-parser.js
@@ -1,11 +1,11 @@
 ﻿(function($) {
 	
 	/**
-	 * Parses an URL and returns an object with protocol, host, port, query and hash.
+	 * Parses an URL and returns an object with protocol, host, port, path, query and hash.
 	 * Supported Protocols: http, https and ftp.
 	 * @name urlParser
 	 * @author Peter Müller
-	 * @example $.urlParser('http://test.de:8080?foo=bar#hashme');
+	 * @example $.urlParser('http://test.de:8080/my/path?foo=bar#hashme');
 	 * @depends jQuery 1.4.2
 	 * @returns {object}
 	 */
@@ -14,7 +14,7 @@
 			$.error('No valid data given.');
 		}
 		
-		var protocol, host, port, query, hash;
+		var protocol, host, port, path, query, hash;
 		
 		protocol = url.match(/^(https?|ftp)/gi);
 		protocol = $.isArray(protocol) && protocol.length ? protocol[0] : '';
@@ -22,8 +22,11 @@
 		host = url.replace(/^(https?:|ftp:|)\/\//gi,'').match(/^([\w\.]){6,}/gi);
 		host = $.isArray(host) && host.length ? host[0] : '';
 		
-		port = url.replace(/^((https?:|ftp:|)\/\/)?([\w\.]){6,}/gi,'').match(/^:(\d){2,4}/gi);
+		port = url.replace(/^((https?:|ftp:|)\/\/)?([\w\.]){6,}/gi,'').match(/^:(\d){2,5}/gi);
 		port = $.isArray(port) && port.length ? parseInt(port[0].replace(/^:/gi, ''), 10) : 80;
+		
+		path = url.replace(/^((https?:|ftp:|)\/\/)?([\w\.]){6,}(:\d{2,5})?/gi,'').match(/^([\/\w]*)/gi);
+		path = $.isArray(path) && path.length ? path[0] : '';
 		
 		query = url.match(/\?.*$/gi);
 		query = $.isArray(query) && query.length ? query[0].replace(/^\?/gi,'').replace(/(#(.*)?)?$/,'') : '';
@@ -35,6 +38,7 @@
 			protocol : protocol,
 			host : host,
 			port : port,
+			path : path,
 			query : query,
 			hash : hash
 		};
